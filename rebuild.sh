81e646440d01209a596d4e6c237d9c6135aef1bb
Anuja Tike <tike.anuja4@gmail.com>
2018-06-09 12:40:42 -0700

    Enron rebuild and rerun scripts

--- rebuild.sh
+++ rebuild.sh
@@ -5,8 +5,9 @@ USER=`whoami`
 export CLASSPATH=.:$(hadoop classpath)
 export HADOOP_CLASSPATH=$CLASSPATH
 
-javac -d classes HousesMapper.java
-javac -d classes HousesReducer.java
-jar -cvf Houses.jar -C classes/ .
-javac -classpath $CLASSPATH:Houses.jar -d classes HousesDriver.java
-jar -uvf Houses.jar -C classes/ .
+javac -d classes EnronMapper.java
+javac -d classes EnronReducer.java
+javac -d classes EnronPartitioner.java
+jar -cvf Enron.jar -C classes/ .
+javac -classpath $CLASSPATH:Enron.jar -d classes EnronDriver.java
+jar -uvf Enron.jar -C classes/ .
--- rerun.sh
+++ rerun.sh
@@ -7,8 +7,8 @@ export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native
 export CLASSPATH=.:$(hadoop classpath)
 export HADOOP_CLASSPATH=$CLASSPATH
 
-rm -rf /user/$USER/HOUSES/OUT
+rm -rf /user/$USER/ENRON/OUT
 
 ARGS=$1
 
-hadoop jar Houses.jar Houses.HousesDriver $ARGS /user/$USER/HOUSES/DATA/train.csv /user/$USER/HOUSES/OUT
+hadoop jar Enron.jar Enron.EnronDriver $ARGS /user/$USER/ENRON/DATA /user/$USER/ENRON/OUT
